f3b5570a1fb1d262e5bd575e2031656da7cbca16
JunHyuk <gj7926@naver.com>
2023-08-28 01:30:47 +0900

    validation-V4

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,28 +1,23 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.Range;
-import org.hibernate.validator.constraints.ScriptAssert;
-
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총 합이 10000원을 넘어야 합니다.")
 public class Item {
 
+//    @NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank(message = "공백 안됩니다.") // message 추가해도 됨.
+//    @NotBlank(message = "공백 안됩니다.", groups = {SaveCheck.class, UpdateCheck.class}) // message 추가해도 됨.
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    @Max(9999)
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
